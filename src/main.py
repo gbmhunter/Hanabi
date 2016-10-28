67f8973387a9ddba039df93685274fc5139bd2af
Geoffrey Hunter <gbmhunter@gmail.com>
2016-10-28 23:38:42 +0200

    Started creating an example "real" player.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,20 @@
 from game import Game
+from player_interface import PlayerInterface
+
+class GeoPlayer(PlayerInterface):
+    def __init__(self):
+        super().__init__()
+        print("Creating a Geo player.")
 
 if __name__ == "__main__":
     print("Hanabi simulator started.")
 
     game = Game()
+
+    # Create and register custom players
+    geoPlayer1 = GeoPlayer()
+
+    game.registerPlayer(geoPlayer1)
+
+
 
